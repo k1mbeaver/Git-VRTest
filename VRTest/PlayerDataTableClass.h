0200d07a0d3936152615ddaabaccfb499663690f
ksw4811 <ksw4811@naver.com>
2022-10-23 23:17:16 +0900

    플레이어 데이터 테이블 추가

---
+++ VRTest/PlayerDataTableClass.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
+#include "PlayerDataTableClass.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPlayerDataTable : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FString PlayerName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int PlayerStage;
+};
+
+UCLASS()
+class VRTEST_API APlayerDataTableClass : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlayerDataTableClass();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
