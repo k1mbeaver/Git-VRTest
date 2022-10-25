f4645e6d91bb3b012bacc3a2e0d47a0d5d7a6141
ksw4811 <ksw4811@naver.com>
2022-10-25 22:44:57 +0900

    AI 캐릭터 데이터 테이블 추가

---
+++ VRTest/AIDataTableClass.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
+#include "AIDataTableClass.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAIDataTable : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		USkeletalMesh* AISkeletalMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UParticleSystem* AIHitParticle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UAnimMontage* AIAttackMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TSubclassOf<class UAnimInstance> AIAnimation;
+
+};
+
+
+UCLASS()
+class VRTEST_API AAIDataTableClass : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAIDataTableClass();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
