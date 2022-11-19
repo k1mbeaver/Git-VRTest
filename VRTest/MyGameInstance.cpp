20cedeb97d0215dd15504ee448859e2e100f1b21
ksw4811 <ksw4811@naver.com>
2022-11-19 16:38:45 +0900

    데이터 테이블 구체적으로 정리

--- VRTest/MyGameInstance.cpp
+++ VRTest/MyGameInstance.cpp
@@ -3,6 +3,10 @@
 
 #include "MyGameInstance.h"
 #include "PlayerDataTableClass.h"
+#include "SoundDataTableClass.h"
+#include "ParticleDataTableClass.h"
+#include "AnimationDataTableClass.h"
+#include "AIDataTableClass.h"
 
 UMyGameInstance::UMyGameInstance()
 {
@@ -14,4 +18,108 @@ UMyGameInstance::UMyGameInstance()
 	{
 		FPlayerFileTable = DT_MYPLAYERFILE.Object;
 	}
+
+	FString ParticleFileDataPath = TEXT("DataTable'/Game/DataTable/ParticleDataTable.ParticleDataTable'");
+
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_MYPARTICLEFILE(*ParticleFileDataPath);
+
+	if (DT_MYPARTICLEFILE.Succeeded())
+	{
+		FParticleFileTable = DT_MYPARTICLEFILE.Object;
+	}
+
+	FString AnimationFileDataPath = TEXT("DataTable'/Game/DataTable/AnimationDataTable.AnimationDataTable'");
+
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_MYANIMATIONFILE(*AnimationFileDataPath);
+
+	if (DT_MYANIMATIONFILE.Succeeded())
+	{
+		FAnimationFileTable = DT_MYANIMATIONFILE.Object;
+	}
+
+	FString SoundFileDataPath = TEXT("DataTable'/Game/DataTable/SoundDataTable.SoundDataTable'");
+
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_MYSOUNDFILE(*SoundFileDataPath);
+
+	if (DT_MYSOUNDFILE.Succeeded())
+	{
+		FSoundFileTable = DT_MYSOUNDFILE.Object;
+	}
+
+	FString AIFileDataPath = TEXT("DataTable'/Game/DataTable/AIDataTable.AIDataTable'");
+
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_MYAIFILE(*AIFileDataPath);
+
+	if (DT_MYAIFILE.Succeeded())
+	{
+		FAIFileTable = DT_MYAIFILE.Object;
+	}
+}
+
+USoundWave* UMyGameInstance::GetSound(FString SoundType)
+{
+	FSoundDataTable* SoundData = FSoundFileTable->FindRow<FSoundDataTable>(*SoundType, TEXT(""));
+	USoundWave* GetMySound = SoundData->MySound;
+	return GetMySound;
+}
+
+UAnimMontage* UMyGameInstance::GetMontage(FString MontageType)
+{
+	FAnimationDataTable* MontageData = FAnimationFileTable->FindRow<FAnimationDataTable>(*MontageType, TEXT(""));
+	UAnimMontage* GetMyMontage = MontageData->MyMontage;
+	return GetMyMontage;
+}
+
+TSubclassOf<class UAnimInstance> UMyGameInstance::GetAnimation(FString AnimationType)
+{
+	FAnimationDataTable* AnimationData = FAnimationFileTable->FindRow<FAnimationDataTable>(*AnimationType, TEXT(""));
+	TSubclassOf<class UAnimInstance> GetMyAnimation = AnimationData->MyAnimation;
+	return GetMyAnimation;
+}
+
+UParticleSystem* UMyGameInstance::GetParticle(FString ParticleType)
+{
+	FParticleDataTable* ParticleData = FParticleFileTable->FindRow<FParticleDataTable>(*ParticleType, TEXT(""));
+	UParticleSystem* GetMyParticle = ParticleData->MyParticle;
+	return GetMyParticle;
+}
+
+FString UMyGameInstance::GetPlayerName(FString PlayerType)
+{
+	FPlayerDataTable* PlayerData = FPlayerFileTable->FindRow<FPlayerDataTable>(*PlayerType, TEXT(""));
+	FString GetPlayerName = PlayerData->PlayerName;
+	return GetPlayerName;
+}
+
+int UMyGameInstance::GetPlayerStage(FString PlayerType)
+{
+	FPlayerDataTable* PlayerData = FPlayerFileTable->FindRow<FPlayerDataTable>(*PlayerType, TEXT(""));
+	int GetPlayerStage = PlayerData->PlayerStage;
+	return GetPlayerStage;
+}
+
+void UMyGameInstance::SetPlayerName(FString PlayerType, FString SetPlayerName)
+{
+	FPlayerDataTable* PlayerData = FPlayerFileTable->FindRow<FPlayerDataTable>(*PlayerType, TEXT(""));
+	PlayerData->PlayerName = SetPlayerName;
+}
+
+void UMyGameInstance::SetPlayerStage(FString PlayerType, int SetPlayerStage)
+{
+	FPlayerDataTable* PlayerData = FPlayerFileTable->FindRow<FPlayerDataTable>(*PlayerType, TEXT(""));
+	PlayerData->PlayerStage = SetPlayerStage;
+}
+
+float UMyGameInstance::GetAISpeed(FString AIType)
+{
+	FAIDataTable* AIData = FAIFileTable->FindRow<FAIDataTable>(*AIType, TEXT(""));
+	float GetAISpeed = AIData->AICharacterSpeed;
+	return GetAISpeed;
+}
+
+FString UMyGameInstance::GetAIType(FString AIType)
+{
+	FAIDataTable* AIData = FAIFileTable->FindRow<FAIDataTable>(*AIType, TEXT(""));
+	FString GetAIType = AIData->AICharacterType;
+	return GetAIType;
 }
--- VRTest/MyGameInstance.h
+++ VRTest/MyGameInstance.h
@@ -10,6 +10,7 @@ struct FPlayerDataTable;
 struct FParticleDataTable;
 struct FAnimationDataTable;
 struct FSoundDataTable;
+struct FAIDataTable;
 
 class UDataTable;
 /**
@@ -20,12 +21,43 @@ class VRTEST_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
-		UMyGameInstance();
-
 public:
+	UMyGameInstance();
 
+	// 사운드 얻어오기
+	USoundWave* GetSound(FString SoundType);
+
+	// 애니메이션, 몽타주 얻어오기
+	UAnimMontage* GetMontage(FString MontageType);
+	TSubclassOf<class UAnimInstance> GetAnimation(FString AnimationType);
+
+	// 파티클 얻어오기
+	UParticleSystem* GetParticle(FString ParticleType);
+
+	// 플레이어 정보 얻어오기
+	FString GetPlayerName(FString PlayerType);
+	int GetPlayerStage(FString PlayerType);
+	void SetPlayerName(FString PlayerType, FString SetPlayerName);
+	void SetPlayerStage(FString PlayerType, int SetPlayerStage);
+
+	// AI 정보 얻어오기
+	float GetAISpeed(FString AIType);
+	FString GetAIType(FString AIType);
+	
 private:
 	UPROPERTY()
 		UDataTable* FPlayerFileTable;
+
+	UPROPERTY()
+		UDataTable* FParticleFileTable;
+
+	UPROPERTY()
+		UDataTable* FAnimationFileTable;
+
+	UPROPERTY()
+		UDataTable* FSoundFileTable;
+
+	UPROPERTY()
+		UDataTable* FAIFileTable;
 
 };
