4fdd6f3e64f6a00b94b4a784cbee407e827a36a1
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2024-03-05 23:45:24 +0900

    Add files via upload
    튜토리얼창에서 플레이어에게 안내를 할 수 있도록 구현진행중...

---
+++ VRTest/Tutorial_UW.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Tutorial_UW.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRTEST_API UTutorial_UW : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
