86e4a4d19ddf31ac7b811572b4e1546a01f12e50
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2024-03-08 23:18:14 +0900

    Add files via upload
    1. 캐릭터 클래스에선 신호
    2. PlayerUI 클래스에선 그 신호를 받아TutorialUI의 함수를 실행하거나 컨트롤
    3. TutorialUI에서는 기능과 관련한 함수를 만들기

---
+++ MyCharacter.h
@@ -0,0 +1,198 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UENUM(BlueprintType)
+enum class EMyPlayerState : uint8
+{
+	E_Start = 0 UMETA(DisplayName = "Start"),
+	E_Ready = 1 UMETA(DisplayName = "Ready"),
+	E_Play = 2 UMETA(DisplayName = "Play"),
+	E_Paused = 3 UMETA(DisplayName = "Pause"),
+	E_Exit = 4 UMETA(DisplayName = "Exit"),
+	E_Test = 5 UMETA(DisplayName = "Test"),
+
+};
+
+DECLARE_MULTICAST_DELEGATE(FOnPlayerMenuDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnPlayerMenuUpDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnPlayerMenuDownDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnPlayerMenuClickDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnPlayerTutorialNextDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnPlayerTutorialPrevDelegate);
+
+UCLASS()
+class VRTEST_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	int TotalMonsterKill;
+	int CurrentMonsterKill;
+	int PlayerStage;
+	FVector PlayerPosition;
+	float GroundPosition;
+	class APlayerGround* myGround;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Motion)
+		class UMotionControllerComponent* MotionLeft;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Motion)
+		class UMotionControllerComponent* MotionRight;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Motion)
+		class USkeletalMeshComponent* MeshLeft;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Motion)
+		class USkeletalMeshComponent* MeshRight;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Motion)
+		class AReadyObject* MyReadyObject;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Camera)
+		class UCameraComponent* MyCamera;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Camera)
+		class USceneComponent* CameraHolder;
+
+
+	bool IsRightGrip;
+	bool IsLeftGrip;
+	bool IsMenuOn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Attack, Meta = (AllowPrivateAccess = true))
+		USoundWave* StartVRSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Attack, Meta = (AllowPrivateAccess = true))
+		USoundWave* StageChangeSound;
+
+	// 현재 플레이어의 상태 (시작화면, 준비화면, 플레이)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		EMyPlayerState MyPlayerState;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool PlayerHasRightObject;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool PlayerHasLeftObject;
+
+	UPROPERTY()
+		class UGameplayStatics* GameStatic;
+
+	class UPlayerAnim* LeftHandInstance;
+	class UPlayerAnim* RightHandInstance;
+
+	class UMyGameInstance* MyGameInstance;
+
+	FOnPlayerMenuDelegate OnPlayerMenuDelegate;
+	FOnPlayerMenuUpDelegate OnPlayerMenuUpDelegate;
+	FOnPlayerMenuDownDelegate OnPlayerMenuDownDelegate;
+	FOnPlayerMenuClickDelegate OnPlayerMenuClickDelegate;
+	FOnPlayerTutorialNextDelegate OnPlayerTutorialNextDelegate;
+	FOnPlayerTutorialPrevDelegate OnPlayerTutorialPrevDelegate;
+
+	UFUNCTION(BlueprintCallable)
+		void PressedMenuButton();
+
+	UFUNCTION(BlueprintCallable)
+		void PressedMenuUpButton();
+
+	UFUNCTION(BlueprintCallable)
+		void PressedMenuDownButton();
+
+	UFUNCTION(BlueprintCallable)
+		void PressedMenuClickButton();
+
+	UFUNCTION(BlueprintCallable)
+		void PressedTutorialNextButton();
+
+	UFUNCTION(BlueprintCallable)
+		void PressedTutorialPrevButton();
+
+	UFUNCTION(BlueprintCallable)
+	void PressedLeftGrip();
+
+	UFUNCTION(BlueprintCallable)
+	void ReleasedLeftGrip();
+
+	UFUNCTION(BlueprintCallable)
+	void PressedRightGrip();
+
+	UFUNCTION(BlueprintCallable)
+	void ReleasedRightGrip();
+
+	UFUNCTION(BlueprintCallable)
+		void SetIsRightGrip();
+
+	UFUNCTION(BlueprintCallable)
+		void SetIsLeftGrip();
+
+	UFUNCTION(BlueprintCallable)
+		void PlayerDead();
+
+	UFUNCTION(BlueprintCallable)
+		void PositionUp();
+
+	UFUNCTION(BlueprintCallable)
+		void PositionDown();
+
+	UFUNCTION(BlueprintCallable)
+		void PositionGo();
+
+	UFUNCTION(BlueprintCallable)
+		void PositionBack();
+
+	UFUNCTION(BlueprintCallable)
+		void PositionLeft();
+
+	UFUNCTION(BlueprintCallable)
+		void PositionRight();
+
+	// 몬스터를 킬했을 시에 호출하는 함수
+	UFUNCTION(BlueprintCallable)
+		void KillingMonster();
+
+	// 플레이어가 게임 중인지 아닌지 확인하는 함수
+	UFUNCTION(BlueprintCallable)
+		bool PlayerPlaying();
+
+	// 플레이어의 게임 Start
+	UFUNCTION(BlueprintCallable)
+		void PlayerGameStart();
+
+
+	// 이벤트
+	UFUNCTION(BlueprintNativeEvent)
+		void OnAllKillMonster();
+	virtual void OnAllKillMonster_Implementation()
+	{
+
+	}
+
+	// 이벤트
+	UFUNCTION(BlueprintNativeEvent)
+		void OnPlayerKillMonster();
+	virtual void OnPlayerKillMonster_Implementation()
+	{
+
+	}
+};
---
+++ Tutorial_UW.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Tutorial_UW.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRTEST_API UTutorial_UW : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(Meta = (BindWidget))
+		class UTextBlock* TextInfo;
+
+	UPROPERTY(Meta = (BindWidget))
+		class UImage* BGImage;
+
+protected:
+	virtual void NativeOnInitialized() override;
+	virtual void NativeConstruct() override;
+
+	float LerpFun(float beginValue, float endValue, float t) 
+	{
+		return beginValue * (1 - t) + (endValue * t);
+	};
+
+	void TransparencyImage(float fSpeed, float fBegin, float fEnd);
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+		void VisibleBackImage(bool bVisible);
+	/*
+	bool IsMenuOn;
+	// KillingPoint ฐüทร
+	void SetCurrentKill();
+	void VisibleKillingPoint();
+	void HiddenKillingPoint();
+	*/
+};
